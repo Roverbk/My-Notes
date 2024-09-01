5fbc3602f45d2b646b23f96a7b858f48c9c41790
Bhakti Kalyankasture <95176364+Roverbk@users.noreply.github.com>
2024-09-01 15:55:14 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import './styles.css'; // Updated path after moving styles.css to src
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
